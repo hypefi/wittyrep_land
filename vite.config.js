9df5d7248c43a00c50ea736b256d9cec33f11d64
Boukharta <anasboukharta@gmail.com>
2025-07-20 17:48:24 +0100

    chaging vite config for blog

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        'blog-post-2025-06-25': resolve(__dirname, 'posts/blog_post_2025-06-25.html'),
+        'blog-whatsapp-automation-smb': resolve(__dirname, 'posts/blog-whatsapp-automation-smb.html')
+      }
+    }
+  }
+}) 
