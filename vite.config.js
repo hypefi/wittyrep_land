ebc2a1c38c85720c17ede13d2dbad37832612fcf
Boukharta <anasboukharta@gmail.com>
2025-08-01 11:04:17 +0100

    404

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         blog: resolve(__dirname, 'blog.html'),
+        '404': resolve(__dirname, '404.html'),
         'blog-post-2025-06-25': resolve(__dirname, 'posts/blog_post_2025-06-25.html'),
         'blog-whatsapp-automation-smb': resolve(__dirname, 'posts/blog-whatsapp-automation-smb.html')
       }
