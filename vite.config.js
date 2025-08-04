17d8b46e3d96adba1e76bfc292e41d4066ee2a38
Boukharta <anasboukharta@gmail.com>
2025-08-04 12:24:34 +0100

    lang

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,13 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         blog: resolve(__dirname, 'blog.html'),
         '404': resolve(__dirname, '404.html'),
+        ar: resolve(__dirname, 'ar.html'),
+        fr: resolve(__dirname, 'fr.html'),
         'blog-post-2025-06-25': resolve(__dirname, 'posts/blog_post_2025-06-25.html'),
-        'blog-whatsapp-automation-smb': resolve(__dirname, 'posts/blog-whatsapp-automation-smb.html')
+        'blog-whatsapp-automation-smb': resolve(__dirname, 'posts/blog-whatsapp-automation-smb.html'),
+        'blog-cost-manual-whatsapp-management': resolve(__dirname, 'posts/blog-cost-manual-whatsapp-management.html'),
+        'blog-real-estate-whatsapp-automation': resolve(__dirname, 'posts/blog-real-estate-whatsapp-automation.html'),
+        'blog-whatsapp-lead-generation-sequences': resolve(__dirname, 'posts/blog-whatsapp-lead-generation-sequences.html')
       }
     }
   }
