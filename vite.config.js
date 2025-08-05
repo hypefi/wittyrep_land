bc9f9fc22eed55909d03702deb970d1128cc686e
Boukharta <anasboukharta@gmail.com>
2025-08-05 11:50:13 +0100

    fixing ar fr

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
         '404': resolve(__dirname, '404.html'),
         ar: resolve(__dirname, 'ar.html'),
         fr: resolve(__dirname, 'fr.html'),
+        'ar/index': resolve(__dirname, 'ar/index.html'),
+        'fr/index': resolve(__dirname, 'fr/index.html'),
         'blog-post-2025-06-25': resolve(__dirname, 'posts/blog_post_2025-06-25.html'),
         'blog-whatsapp-automation-smb': resolve(__dirname, 'posts/blog-whatsapp-automation-smb.html'),
         'blog-cost-manual-whatsapp-management': resolve(__dirname, 'posts/blog-cost-manual-whatsapp-management.html'),
